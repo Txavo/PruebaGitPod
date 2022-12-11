ceac8a350a87450f9561a2b355a5a4e0f1ed6987
Txavo <jrmachin@gmail.com>
2022-12-11 10:23:09 +0000

    prueba

--- prueba.py
+++ prueba.py
@@ -1,1 +1,16 @@
-print('hola mundo...')
+# Prueba con clases
+
+class empleado:
+    def __init__(self, identificador, nombre, edad):
+        self.id     = identificador
+        self.nombre = nombre
+        self.edad   = edad
+    
+    def get_data(self):
+        print('El empleado con ID = {} de nombre {} tiene {} años'.format(self.id, self.nombre, self.edad))
+
+
+a = empleado(1, 'Juan', 25)
+
+a.get_data()
+
